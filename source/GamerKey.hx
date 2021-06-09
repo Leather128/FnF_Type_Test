f3f895f1372a36ba3995dddce23e08a7bcd7fad5
Leather128 <gwr.yt2@gmail.com>
2021-06-09 15:40:52 -0400

    added gamer key lol

---
+++ source/GamerKey.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxSprite;
+
+class GamerKey extends FlxSprite
+{
+    public function new(x:Float, y:Float, character:String = "a")
+    {
+        super(x, y);
+
+        antialiasing = false;
+		frames = Paths.getSparrowAtlas('keys/keys');
+
+        animation.addByPrefix(character, character, 1);
+
+        animation.play(character, true, false, 0);
+    }
+}
